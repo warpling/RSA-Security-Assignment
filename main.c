c833bdf3d0f0270cf77694a12ce485961f197dd1
Ryan McLeod <ryanmcleod@gmail.com>
2014-01-28 19:55:54 -0800

    init

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <gmp.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    gmp_randstate_t state;
+    unsigned long int i, seed = 12345;
+    // Get a timestamp and use it as our seed
+    // (this is generally regarded as a 'shitty idea')
+    // time_t nowTime = time(0);
+    // seed = (unsigned long int) rawTime;
+    // Initiate random state
+    gmp_randinit_default(seed);
+    gmp_randseed_ui(state, seed);
+
+    // ~*magic*~
+    mpz_t randNum;
+    mpz_init (randNum);
+    mpz_urandomb (randNum, state, 2);
+
+    // Loop and print
+    for(i = 0; i < 10; ++i) {
+       mpz_urandomb(randNum, state, 4);
+       gmp_printf("%Zd\n", randNum);
+    }
+
+    // Clean up, clean up, everybody do your chore...
+    gmp_randclear(state);
+    mpz_clear(randNum);
+    return 0;
+}
