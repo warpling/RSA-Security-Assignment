2d2d0b712ea2bbcde3de67e7f7e5df83f413bdf0
drschulz <daschulz@calpoly.edu>
2014-02-07 16:09:27 -0800

    cudafunctions ready for testing I hope

--- cudaFunctions.cu
+++ cudaFunctions.cu
@@ -1,22 +1,35 @@
 #include "cudaFunctions.h"
 
-__global__ void gcdKernel(int ind1, int ind2, int totNumKeys, BigInt keys[], int res) {
+__global__ void gcdKernel(int base, int offset, BigInt keys[], int numKeys, char results[]) {
    
-   int row = blockIdx.y*blockDim.y + threadIdx.y;
-   int b, i;
-   __shared__ int results[BLOCKDIM_Y][BLOCKDIM_X];
-   BigInt n = keys[ind1+row];
-   BigInt tmp = n;
-   BigInt m; 
+   int key1 = base + threadIdx.y;
+   int key2 = offset + blockIdx.x;
 
-   for(i = ind1+row + 1; i < totNumKeys; i++) {
+   __shared__ BigInt sharedkeys[BLOCKDIM_Y][2];
+   BigInt *res;
+   
+   //__shared__ int results[BLOCKDIM_Y][BLOCKDIM_X];
+   
+   if(key1 < key2 && key2 < numKeys && key1 < numKeys) {
+      sharedkeys[threadIdx.y][1] = keys[key1];
+      sharedkeys[threadIdx.y][2] = keys[key2];
+      res = gcd(&sharedkeys[threadIdx.y][1], &sharedkeys[threadIdx.y][2]);
+      if(notOne(*res)) {
+         results[key1*gridDim.x + key2] = 1;
+      }
+   }
+   //BigInt n = keys[ind1+row];
+   //BigInt tmp = n;
+   //BigInt m; 
+   
+   /*for(i = ind1+row + 1; i < totNumKeys; i++) {
       m = keys[i];
       m = gcd(tmp, m);
       if(notOne(m)) {
          res[totNumKeys*(ind1+row) + i] = 1;
       }
 
-   }
+   }*/
 }
 
 __device__ void  shiftR(BigInt *n) {
@@ -24,11 +37,11 @@ __device__ void  shiftR(BigInt *n) {
    int id = threadIdx.x;
 
    if(id != 31) {
-      part = n.elements[threadIdx.x + 1];
+      part = n.components[threadIdx.x + 1];
    }
-   n.elements[threadIdx.x] = (n.elements[threadIdx.x] >> 1) | (part << 31);
+   n.components[threadIdx.x] = (n.components[threadIdx.x] >> 1) | (part << 31);
    
-   /*unsigned int part = n->elements[threadIdx.x];
+   /*unsigned int part = n->components[threadIdx.x];
    __shared__ unsigned int rsResult[BLOCKDIM_Y][BLOCKDIM_X] = {{0}};
    unsigned int mask = 1;
 
@@ -42,7 +55,7 @@ __device__ void  shiftR(BigInt *n) {
       rsResult[threadIdx.y][threadIdx.x] |= part;
    }
    __syncThreads();
-   n->elements[threadIdx.x] = rsResult[threadIdx.y][threadIdx.x];
+   n->components[threadIdx.x] = rsResult[threadIdx.y][threadIdx.x];
    __syncthreads();*/
 }
 
@@ -50,33 +63,33 @@ __device__ void  shiftL(BigInt *n) {
    unsigned int part = 0;
 
    if(ThreadIdx.x) {
-      part = n.elements[threadIdx.x - 1];
+      part = n.components[threadIdx.x - 1];
    }
-   n.elements[threadIdx.x] = (n.elements[threadIdx.x] << 1) | (part >> 31);
+   n.components[threadIdx.x] = (n.components[threadIdx.x] << 1) | (part >> 31);
 
-   /*unsigned int part = n->elements[threadIdx.x];
+   /*unsigned int part = n->components[threadIdx.x];
    __shared__ unsigned int lOverflow[BLOCKDIM_Y][BLOCKDIM_X];
    unsigned int mask = 1 << 31;
 
    if(threadIdx.x != blockDim.x - 1) {
-      lOverflow.elements[threadIdx.y][threadIdx.x - 1] = (part & mask) >> 31;
+      lOverflow.components[threadIdx.y][threadIdx.x - 1] = (part & mask) >> 31;
    }
 
    part <<= 1;
    __syncthreads();
 
    if(threadIdx.x != 0)
-      part = part | lOverflow.elements[threadIdx.y][threadIdx.x];
+      part = part | lOverflow.components[threadIdx.y][threadIdx.x];
    }
 
-   n->elements[threadIdx.x] = part;
+   n->components[threadIdx.x] = part;
    __syncthreads();*/
 }
 
-__device__ void cuSubtract(BigInt *n, BigInt *m, unsigned int *res) {
+__device__ void cuSubtract(BigInt *n, BigInt *m) {
 
-   unsigned int partn = n->elements[threadIdx.x];
-   unsigned int partm = m->elements[threadIdx.x];
+   unsigned int partn = n->components[threadIdx.x];
+   unsigned int partm = m->components[threadIdx.x];
    __shared__ unsigned int borrowArray[BLOCKDIM_Y][BLOCKDIM_X] = {{0}};
    //__shared__ short okayToGo[BLOCKDIM_Y][BLOCKDIM_X] = {{0}};
    //unsigned int result = 0;
@@ -103,8 +116,11 @@ __device__ void cuSubtract(BigInt *n, BigInt *m, unsigned int *res) {
       if(threadIdx.x != 31 && t == 0xffffffffU) {
          borrowArray[threadIdx.y][threadIdx.x + 1] = 1;
       }
+      else {
+         borrowArray[threadIdx.y][threadIdx.x + 1] = 0;
+      }
    }
-   n->elements[threadIdx.x] = tmp;
+   n->components[threadIdx.x] = tmp;
 
 
 
@@ -144,16 +160,31 @@ __device__ void cuSubtract(BigInt *n, BigInt *m, unsigned int *res) {
 
 __device__ bool geq(BigInt *n, BigInt *m) {
 
-   /*keep a shared mem array that contains flags for thread actions*/
+   __shared__ int differPos[BLOCKDIM_Y];
+   int *pos = differPos + threadIdx.y;
+
+   if(threadIdx.x == 0) {
+      differPos[threadIdx.y] = 0;
+   }
+
+   if(n->components[threadIdx.x] != m->components[threadIdx.x]) {
+      atomicMax(pos, threadIdx.x);
+   }
+
+   return n->components[*pos] >= m->components[*pos];
+}
+
+
+   /*keep a shared mem array that contains flags for thread actions
    __shared__ short gThan[BLOCKDIM_Y][BLOCKDIM_X] = {{0}};
    short res;
 
-   /*Set the Most significant thread to a val to start the process*/
+   /*Set the Most significant thread to a val to start the process
    if(threadIdx.x == BLOCKDIM_X - 1) {
       gThan[threadIdx.y][threadIdx.x] = NOTSURE;
    }
 
-   /*Loop the threads until a flag is set for them*/
+   /*Loop the threads until a flag is set for them
    while(!gThan[threadIdx.y][threadIdx.x]){};
 
    res = gThan[threadIdx.y][threadIdx.x];
@@ -161,11 +192,9 @@ __device__ bool geq(BigInt *n, BigInt *m) {
    /* If a more significant thread found out that the numbers
     * were greater, notify the less significant threads and
     * return.
-    */
    if(res == ISGREATER) {
       /* The least significant thread doesn't need to
        * notify anyone.
-       */
       if(threadIdx.x != 0) {
          gThan[threadIdx.y][threadIdx.x - 1] = ISGREATER;
       }
@@ -176,11 +205,9 @@ __device__ bool geq(BigInt *n, BigInt *m) {
    /* If a more significant thread found out that the numbers
     * were not greater, notify the less significant threads and
     * return.
-    */
    if(res == NOTGREATER) {
       /* The least significant thread doesn't need to
        * notify anyone.
-       */
       if(threadIdx.x != 0) {
          gThan[threadIdx.y][threadIdx.x - 1] = ISGREATER;
       }
@@ -191,13 +218,11 @@ __device__ bool geq(BigInt *n, BigInt *m) {
    /* If a more significant thread was unsure whether the
     * numbers were greater than or not, check the numbers
     * handled by THIS current thread.
-    */
    if(res == NOTSURE) {
       /* If the numbers handled by this thread show n is greater
        * than m, then notify the more significant threads, and
        * the lesser significant threads, and return.
-       */
-      if(n->elements[threadIdx.x] > m->elements[threadIdx.x]) {
+      if(n->components[threadIdx.x] > m->components[threadIdx.x]) {
          if(threadIdx.x != BLOCKDIM_X - 1) {
             gThan[threadIdx.y][threadIdx.x + 1] = NOTIFY_IS_GREATER;
          }
@@ -210,8 +235,7 @@ __device__ bool geq(BigInt *n, BigInt *m) {
       /* If the numbers handled by this thread show n is not greater
        * than m, then notify the more significant threads, and
        * the lesser significant threads, and return.
-       */
-      else if(n->elements[threadIdx.x] < m->elements[threadIdx.x]) {
+      else if(n->components[threadIdx.x] < m->components[threadIdx.x]) {
          if(threadIdx.x != BLOCKDIM_X - 1) {
             gThan[threadIdx.y][threadIdx.x + 1] = NOTIFY_NOT_GREATER;
          }
@@ -221,11 +245,10 @@ __device__ bool geq(BigInt *n, BigInt *m) {
          return false;
       }
 
-      /* If the numbers handled by this thread are equal ...*/
+      /* If the numbers handled by this thread are equal ...
       else {
          /* If this thread is the least significant thread, 
           * notify all other threads and return.
-          */
          if(threadIdx.x == 0) {
             gThan[threadIdx.y][threadIdx.x + 1] = NOTIFY_IS_GREATER;
             return true;
@@ -233,7 +256,6 @@ __device__ bool geq(BigInt *n, BigInt *m) {
          
          /* If the number is not the least significant thread,
           * tell the next less significant thread it is unsure.
-          */
          gThan[threadIdx.y][threadIdx.x - 1] = NOTSURE;
 
       }
@@ -243,7 +265,6 @@ __device__ bool geq(BigInt *n, BigInt *m) {
    
    /* Wait for the lesser significant threads to notify this
     * thread of an answer if it was unsure.
-    */
    while(res != NOTIFY_IS_GREATER || res != NOTIFY_NOT_GREATER) {
       res = gThan[threadIdx.y][threadIdx.x];
    }
@@ -254,20 +275,19 @@ __device__ bool geq(BigInt *n, BigInt *m) {
    }
    
    gThan[threadIdx.y][threadIdx.x + 1] = NOTIFY_NOT_GREATER;
-   return false;
-}
+   return false;*/
  
 __device__ bool notOne(BigInt n) {
    __shared__ bool notOne[BLOCKDIM] = {{false}};
    int ind = threadIdx.x;
    int key = threadIdx.y;
 
-   if(ind != 0 && n.elements[ind] != 0) {
+   if(ind != 0 && n.components[ind] != 0) {
       notOne[key] = true;
    }
-   if(ind == 0 && n.elements[ind] != 1) {
+   if(ind == 0 && n.components[ind] != 1) {
       notOne[key] = true;
-   __syncthreads();
+   }
 
    return notOne[key];
 }
@@ -276,66 +296,53 @@ __device__ bool notZero(BigInt n) {
    __shared__ bool isZero[BLOCKDIM] = {{true}};
    int ind = threadIdx.x;
 
-   if(n.elements[ind] != 0) {
+   if(__any(threadIdx.x)) {
+      return false;
+   }
+   /*if(n.components[ind] != 0) {
       isZero[threadIdx.y] = false;
    }
    __syncthreads();
 
-   return isZero[threadIdx.y];
+   return isZero[threadIdx.y];*/
 }
 
-__device__ BigInt gcd(BigInt n, BigInt m) {
+__device__ BigInt* gcd(BigInt *n, BigInt *m) {
 
    int i;
    unsigned int tmp;
+   BigInt *tmpptr;
 
-   for(i = 0; ((n.elements[0] | m.elements[0]) & 1) == 0; i++) {
-      shiftR(&n);
-      shiftR(&m);
+   for(i = 0; ((n->components[0] | m->components[0]) & 1) == 0; i++) {
+      shiftR(n);
+      shiftR(m);
    }
 
-   tmp = n.elements[0];
+   tmp = n->components[0];
 
    while ((tmp & 1) == 0) {
-      shiftR(&n);
-      tmp = n.elements[0];
+      shiftR(n);
+      tmp = n->components[0];
    }
 
    do {
-      while((m.elements[0] & 1) == 0) {
-         shiftR(&m);
+      while((m->components[0] & 1) == 0) {
+         shiftR(m);
       }
 
-      if(geq(&n, &m)) {
-         tmp = n.elements[threadIdx.x];
-         n.elements[threadIdx.x] = m.elements[threadIdx.x];
-         m.elements[threadIdx.x] = tmp;
+      if(geq(n, m)) {
+         tmpptr = n->components;
+         n->components= m->components;
+         m->components = tmpptr;
       }
-      __syncthreads();
 
-      cuSub(n, m);
-   } while(notZero(n));
+      cuSub(m, n);
+   } while(notZero(*n));
    while(i != 0) {
-      shiftL(&m);
+      shiftL(m);
       i--;
    }
    
    return m;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
